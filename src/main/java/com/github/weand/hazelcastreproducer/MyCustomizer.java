e3723ea668eaa2543e81f537119386983a9d1b87
Andreas Weise <a.weise@avm.de>
2025-10-24 23:22:54 +0200

    use io.quarkus.vertx.VertxOptionsCustomizer

--- src/main/java/com/github/weand/hazelcastreproducer/MyCustomizer.java
+++ src/main/java/com/github/weand/hazelcastreproducer/MyCustomizer.java
@@ -1,37 +1,46 @@
+/*
+ * Copyright 2025 by AVM GmbH
+ */
+
 package com.github.weand.hazelcastreproducer;
 
 import static com.github.weand.hazelcastreproducer.HazelcastReproducer.CACHE_NAME;
 import static com.github.weand.hazelcastreproducer.HazelcastReproducer.CLUSTER_NAME;
+import static io.vertx.spi.cluster.hazelcast.ConfigUtil.loadConfig;
 
 import com.github.weand.hazelcastreproducer.task.Task;
 import com.github.weand.hazelcastreproducer.task.TaskSerializer;
-import com.google.auto.service.AutoService;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.ReplicatedMapConfig;
 import com.hazelcast.config.SerializationConfig;
 import com.hazelcast.config.SerializerConfig;
-
-import io.vertx.core.spi.VertxServiceProvider;
+import io.quarkus.vertx.VertxOptionsCustomizer;
+import io.vertx.core.VertxOptions;
 import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import jakarta.enterprise.context.ApplicationScoped;
 
+/**
+ * @author Andreas Weise (a.weise@avm.de)
+ */
+@ApplicationScoped
+public class MyCustomizer implements VertxOptionsCustomizer {
 
-@AutoService(VertxServiceProvider.class)
-public class CustomClusterManager extends HazelcastClusterManager {
+    @Override
+    public void accept(VertxOptions options) {
+        final Config hcConfig = loadConfig()
+            .setClusterName(CLUSTER_NAME)
+            .setInstanceName(CLUSTER_NAME);
 
-    public CustomClusterManager() {
-        final Config hcConfig = loadConfig(); // read hazelcast-default.xml as base config
-        hcConfig.setInstanceName(CLUSTER_NAME);
-        
         final SerializationConfig serialConf = hcConfig.getSerializationConfig();
         serialConf.addSerializerConfig(new SerializerConfig()
             .setImplementation(new TaskSerializer())
             .setTypeClass(Task.class));
-        
+
         final ReplicatedMapConfig taskCacheConfig = new ReplicatedMapConfig(CACHE_NAME);
         taskCacheConfig.setAsyncFillup(false);
         hcConfig.addReplicatedMapConfig(taskCacheConfig);
-        
-        setConfig(hcConfig);
+
+        options.setClusterManager(new HazelcastClusterManager(hcConfig));
     }
 
 }
